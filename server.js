eabbcef624ffcac0aa13e060b2fcf9fdea0c1bf3
Cooper <eliteswimmer42@gmail.com>
2020-10-06 11:43:50 -0500

    got the view, add, delete working

--- server.js
+++ server.js
@@ -3,12 +3,15 @@
 let express = require("express");
 let path = require("path");
 let fs = require("fs");
+let notes = require("./db/db");
+
+let { nanoid } = require("nanoid");
 
 
 // Sets up the Express App
 // =============================================================
 let app = express();
-let PORT = process.env.PORT || 8000;;
+let PORT = process.env.PORT || 8000;
 
 
 // Sets up the Express app to handle data parsing
@@ -24,24 +27,43 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("./api/notes", function(req, res) {
+app.get("/api/notes", function(req, res) {
   return res.json(notes);
 });
 
 
 // Create New Notes - takes in JSON input
-app.post("./api/notes", function(req, res) {
-  let entry = req.params.notes;
-
-  console.log(entry);
-
-  for (let i = 0; i < notes.length; i++) {
-    if (entry === notes[i].routeName) {
-      return res.json(notes[i]);
+app.post("/api/notes", function(req, res) {
+  let entry = req.body;
+  entry.id = nanoid();
+  notes.push(entry);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", function (err) {
+    if (err) {
+        throw err;
     }
-  }
+    console.log("success!");
+    return res.json(entry);
+  });
+});
 
-  return res.json(false);
+
+// Delete Notes
+app.delete("/api/notes/:id", function(req, res) {
+  var noteID = req.params.id;
+  console.log(noteID);
+  let index = notes.map(x => {
+    return x.id;
+  }).indexOf(noteID);
+
+  notes.splice(index, 1);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), "utf8", function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("success!");
+    return res.json(noteID);
+  });
+
 });
 
 
